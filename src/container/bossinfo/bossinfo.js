c9a0e988d28f1f89e03038125281c39884e76fcd
cuidezhu <chinacuidezhu@gmail.com>
2018-05-05 20:39:16 +0800

    bossinfo

--- src/container/bossinfo/bossinfo.js
+++ src/container/bossinfo/bossinfo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavBar, InputItem, TextareaItem } from 'antd-mobile'
+import { NavBar, InputItem, TextareaItem, Button } from 'antd-mobile'
 import AvatarSelector from '../../components/avatar-selector/avatar-selector'
 
 class BossInfo extends React.Component {
@@ -18,7 +18,13 @@ class BossInfo extends React.Component {
     return (
       <div>
         <NavBar mode="dark">BOSS完善信息页</NavBar>
-        <AvatarSelector></AvatarSelector>
+        <AvatarSelector
+          selectAvatar={(imgname) =>
+            this.setState({
+              avatar: imgname
+            })
+          }
+        ></AvatarSelector>
         <InputItem onChange={(v)=>this.onChange('title',v)}>
           招聘职位
         </InputItem>
@@ -35,6 +41,7 @@ class BossInfo extends React.Component {
           title='职位要求'
         >
         </TextareaItem>
+        <Button type='primary'>保存</Button>
       </div>
     )
   }
