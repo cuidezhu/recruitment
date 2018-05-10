88368c836ebbeaec653eaf580fa0c632f16cb8d5
cuidezhu <chinacuidezhu@gmail.com>
2018-05-10 10:05:59 +0800

    genius list redux

--- src/components/boss/boss.js
+++ src/components/boss/boss.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import axios from 'axios'
 import { Card, WhiteSpace, WingBlank } from 'antd-mobile'
+import { connect } from 'react-redux'
+import { getUserList } from '../../redux/chatuser.redux'
 
+@connect(
+  state=>state.chatuser,
+  { getUserList }
+)
 class Boss extends React.Component {
   constructor(props) {
     super(props)
@@ -10,21 +15,16 @@ class Boss extends React.Component {
     }
   }
   componentDidMount() {
-    axios.get('/user/list?type=genius')
-      .then(res => {
-        if (res.data.code == 0) {
-          this.setState({data:res.data.data})
-        }
-      })
+    this.props.getUserList('genius')
   }
 
   render() {
-    console.log(this.state.data)
     const Header = Card.Header
     const Body = Card.Body
+    console.log(this.props.userlist)
     return (
       <WingBlank>
-        {this.state.data.map(v => (
+        {this.props.userlist.map(v => (
 
           v.avatar ? <Card key={v._id}>
             <Header
