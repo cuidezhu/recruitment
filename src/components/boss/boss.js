94a6b559ffc1464022ffc473e07d7d58945aaffb
cuidezhu <chinacuidezhu@gmail.com>
2018-05-13 14:40:25 +0800

    usercard component optimize

--- src/components/boss/boss.js
+++ src/components/boss/boss.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card, WhiteSpace, WingBlank } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { getUserList } from '../../redux/chatuser.redux'
+import UserCard from '../usercard/usercard'
 
 @connect(
   state=>state.chatuser,
@@ -14,28 +15,7 @@ class Boss extends React.Component {
   }
 
   render() {
-    const Header = Card.Header
-    const Body = Card.Body
-    console.log(this.props.userlist)
-    return (
-      <WingBlank>
-        {this.props.userlist.map(v => (
-
-          v.avatar ? <Card key={v._id}>
-            <Header
-              title={v.usr}
-              thumb={require(`../img/${v.avatar}.png`)}
-              extra={<span>{v.title}</span>}
-            ></Header>
-            <Body>
-              {v.desc.split('\n').map(v => (
-                <div key={v}>{v}</div>
-              ))}
-            </Body>
-          </Card> : null
-        ))}
-      </WingBlank>
-    )
+    return <UserCard userlist={this.props.userlist}></UserCard>
   }
 
 }
