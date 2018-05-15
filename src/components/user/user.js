2b62823edcf33f05014d077db8413e9979a0ea87
cuidezhu <chinacuidezhu@gmail.com>
2018-05-15 08:32:58 +0800

    user center info

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Result, List, WhiteSpace } from 'antd-mobile'
 
 @connect(
   state=>state.user
@@ -7,12 +8,32 @@ import { connect } from 'react-redux'
 class User extends React.Component {
 
   render() {
-    console.log(this.props)
-    return (
+    const props = this.props
+    const Item = List.Item
+    const Brief = Item.Brief
+
+    return props.user ? (
       <div>
-        <p>用户中心页</p>
+        <Result
+          img={<img src={require(`../img/${props.avatar}.png`)} style={{width:50}} alt="avatar"/>}
+          title={props.user}
+          message={props.type === 'boss' ? props.company : null}
+        />
+        <List renderHeader={() => '简介'}>
+          <Item
+            multipleLine
+          >
+            {props.title}
+            {this.props.desc.split('\n').map(v => <Brief key={v}>{v}</Brief>)}
+            {this.props.money ? <Brief>薪资: {props.money}</Brief> : null}
+          </Item>
+        </List>
+        <WhiteSpace></WhiteSpace>
+        <List>
+          <Item>退出登录</Item>
+        </List>
       </div>
-    )
+    ) : null
   }
 }
 
