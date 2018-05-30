c2e5f54bfa70303ddc028b244d38044e0d786c5a
cuidezhu <chinacuidezhu@gmail.com>
2018-05-30 08:12:22 +0800

    chat page

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ import AuthRoute from './components/authroute/authroute'
 import BossInfo from './container/bossinfo/bossinfo'
 import GeniusInfo from './container/geniusinfo/geniusinfo'
 import Dashboard from './components/dashboard/dashboard'
+import Chat from './components/chat/chat'
 
 const store = createStore(reducers, compose(
   applyMiddleware(thunk),
@@ -36,6 +37,7 @@ ReactDom.render(
           <Route path='/geniusinfo' component={GeniusInfo}></Route>
           <Route path='/login' component={Login}></Route>
           <Route path='/register' component={Register}></Route>
+          <Route path='/chat/:user' component={Chat}></Route>
           <Route component={Dashboard}></Route>
         </Switch>
       </div>
