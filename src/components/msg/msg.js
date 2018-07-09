69cffce830459dde567fcd9f3431c73e7add31c0
cuidezhu <chinacuidezhu@gmail.com>
2018-07-09 17:49:38 +0800

    chat list

--- src/components/msg/msg.js
+++ src/components/msg/msg.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
+import { List } from 'antd-mobile'
 @connect(
   state=>state
 )
 class Msg extends React.Component {
+  getLast(arr) {
+    return arr[arr.length - 1]
+  }
   render() {
-    console.log(this.props)
+    const Item = List.Item
+    const Brief = Item.Brief
     const msgGroup = {}
     this.props.chat.chatmsg.forEach(v => {
       msgGroup[v.chatid] = msgGroup[v.chatid] || []
       msgGroup[v.chatid].push(v)
     })
-    console.log(msgGroup)
     // 按照聊天用户分组，根据chatid
+    const chatList = Object.values(msgGroup)
     return (
       <div>
-        <h2>消息列表</h2>
+        <List>
+          {chatList.map(v => {
+            console.log(v)
+            const lastItem = this.getLast(v)
+            return (
+              <Item
+                key={lastItem._id}
+              >
+                {lastItem.content}
+                <Brief>用户名</Brief>
+              </Item>
+            )
+          })}
+        </List>
       </div>
     )
   }
