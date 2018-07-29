62d37cd593642dad1c9d3f982f1ab1aef7538a15
cuidezhu <chinacuidezhu@gmail.com>
2018-07-30 01:01:04 +0800

    html skeleton

--- server/server.js
+++ server/server.js
@@ -78,7 +78,27 @@ app.use(function(req, res, next) {
     </Provider>)
   )
 
-  res.send(markup)
+  const pageHtml = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="theme-color" content="#000000">
+  
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
+      <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+  
+      <title>React App</title>
+    </head>
+    <body>
+      <noscript>
+        You need to enable JavaScript to run this app.
+      </noscript>
+      <div id="root">${markup}</div>
+    </body>
+  </html>`
+
+  res.send(pageHtml)
   
   // return res.sendFile(path.resolve('build/index.html'))
 })
@@ -87,3 +107,4 @@ app.use('/', express.static(path.resolve('build')))
 server.listen(9093, function() {
   console.log('Node app start at port 9093')
 })
+
